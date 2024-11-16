189c6c8c57645fd98399e0037d9f32f34e56cbfc
Hashir mehboob <f2022065252@umt.edu.pk>
2024-11-17 01:33:28 +0500

    authentication service is ready

---
+++ MegaBlogs/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+  return (
+    <>
+    <h1>Hello this project is with AppWrite</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ MegaBlogs/src/Appwrite/auth.js
@@ -0,0 +1,75 @@
+import conf from "../Config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteurl) // Appwrite endpoint
+      .setProject(conf.appwriteProjectId); // Appwrite project ID
+    this.account = new Account(this.client);
+  }
+
+  // Create account function
+  async createAccount({ email, password }) {
+    try {
+      const UserAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password
+      );
+
+      if (UserAccount) {
+        // If account creation is successful, log the user in
+        return this.login({ email, password });
+      } else {
+        return UserAccount;
+      }
+    } catch (error) {
+      console.error("Failed to create account:", error.message);
+      throw new Error("Account creation failed. Please try again.");
+    }
+  }
+
+  // Login function
+  async login({ email, password }) {
+    try {
+      const session = await this.account.createEmailPasswordSession(
+        email,
+        password
+      );
+      return session;
+    } catch (error) {
+      console.error("Failed to login:", error.message);
+      throw new Error("Login failed. Please check your credentials.");
+    }
+  }
+
+  // Log out
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.error("Failed to logout:", error.message);
+      throw new Error("Logout failed. Please try again.");
+    }
+  }
+
+  // Get account
+  async GetCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("AppWrite Error", error);
+    }
+
+    return null;
+  }
+}
+
+// Export an instance of AuthService
+const authservice = new AuthService();
+
+export default authservice;
